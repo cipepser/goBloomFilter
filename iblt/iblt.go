6738faf30321ebdac5d75dd19fbbcadee5c9915d
cipepser <respepic@gmail.com>
2018-03-04 15:24:07 +0900

    implement NewInvertibleBloomLookupTables

--- iblt/iblt.go
+++ iblt/iblt.go
@@ -1,1 +1,52 @@
 package iblt
+
+import (
+	"strconv"
+
+	"github.com/cipepser/goBloomFilter/util"
+)
+
+type cell struct {
+	count, keySum, valueSum int
+}
+
+// InvertibleBloomLookupTables is a slice of cell.
+type InvertibleBloomLookupTables []cell
+
+var (
+	k = 3
+)
+
+// NewInvertibleBloomLookupTables constructs InvertibleBloomLookupTables with table size.
+func NewInvertibleBloomLookupTables(size int) InvertibleBloomLookupTables {
+	return make([]cell, size)
+}
+
+// Insert inserts a key-value pair into InvertibleBloomLookupTables.
+// This operation always succeeds.
+func (iblt InvertibleBloomLookupTables) Insert(key, value int) {
+	hash := util.CalcMD5Hash(strconv.Itoa(key))
+	hashA := hash[:int(len(hash)/2)]
+	hashB := hash[int(len(hash)/2):]
+
+	i64HashA, _ := strconv.ParseInt(hashA, 16, 64)
+	i64HashB, _ := strconv.ParseInt(hashB, 16, 64)
+
+	for i := 0; i < k; i++ {
+		iblt[util.DoubleHashing(i64HashA, i64HashB, i, len(iblt))].count++
+		iblt[util.DoubleHashing(i64HashA, i64HashB, i, len(iblt))].keySum ^= key
+		iblt[util.DoubleHashing(i64HashA, i64HashB, i, len(iblt))].valueSum ^= value
+	}
+}
+
+// func (iblt InvertibleBloomLookupTables) LookUp() {
+//
+// }
+//
+// func (iblt InvertibleBloomLookupTables) Delete() {
+//
+// }
+//
+// func (iblt InvertibleBloomLookupTables) ListUp() {
+//
+// }
--- iblt/iblt_test.go
+++ iblt/iblt_test.go
@@ -1,1 +1,26 @@
 package iblt
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewInvertibleBloomLookupTables(t *testing.T) {
+	type args struct {
+		size int
+	}
+	tests := []struct {
+		name string
+		args args
+		want InvertibleBloomLookupTables
+	}{
+		{"Construct Test", args{10}, make([]cell, 10)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewInvertibleBloomLookupTables(tt.args.size); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewInvertibleBloomLookupTables() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
