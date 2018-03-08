763687625ce953d58f82a170ad5bd1a78d052f43
cipepser <respepic@gmail.com>
2018-03-09 00:10:37 +0900

    add tests for Insert and Get

--- iblt/iblt_test.go
+++ iblt/iblt_test.go
@@ -5,21 +5,57 @@ import (
 	"testing"
 )
 
-func TestNewInvertibleBloomLookupTables(t *testing.T) {
+func TestNewIBLT(t *testing.T) {
 	type args struct {
 		size int
 	}
 	tests := []struct {
 		name string
 		args args
-		want InvertibleBloomLookupTables
+		want IBLT
 	}{
 		{"Construct Test", args{10}, make([]cell, 10)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewInvertibleBloomLookupTables(tt.args.size); !reflect.DeepEqual(got, tt.want) {
+			if got := NewIBLT(tt.args.size); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewInvertibleBloomLookupTables() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIBLT_InsertndGet(t *testing.T) {
+	type args struct {
+		key    int
+		value  int
+		getkey int
+	}
+	tests := []struct {
+		name string
+		iblt IBLT
+		args args
+		want bool
+	}{
+		{
+			"Insert {1, 3} and Get(1)",
+			NewIBLT(10),
+			args{1, 3, 1},
+			true,
+		},
+		{
+			"Insert {1, 3} and Get(2)",
+			NewIBLT(10),
+			args{1, 3, 2},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.iblt.Insert(tt.args.key, tt.args.value)
+			got, _ := tt.iblt.Get(tt.args.getkey)
+			if got != tt.want {
+				t.Errorf("Get(1) = %v, want %v", got, tt.want)
 			}
 		})
 	}
